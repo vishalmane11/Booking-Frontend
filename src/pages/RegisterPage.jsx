04b1485b0c4011ed063f92516bad8655bc62feb4
vishalmane11 <85506357+vishalmane11@users.noreply.github.com>
2024-01-29 22:41:09 +0530

    change default

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -9,7 +9,7 @@ export default function RegisterPage() {
   async function registerUser(ev) {
     ev.preventDefault();
     try {
-      await axios.post("/register", {
+      await axios.post(`${import.meta.env.VITE_API_BASE_URL}/register`, {
         name,
         email,
         password,
