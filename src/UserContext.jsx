e78bc3fbe5b9496a3ae1dff1781adb1ea1be3d7a
vishalmane11 <85506357+vishalmane11@users.noreply.github.com>
2024-01-30 10:51:52 +0530

    dfdf

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -10,7 +10,7 @@ export function UserContextProvider({ children }) {
   useEffect(() => {
     if (!user) {
       axios
-        .get(`https://bookingbackend-tpuz.onrender.com/api/profile`)
+        .get(`https://bookingbackend-tpuz.onrender.com/profile`)
         .then(({ data }) => {
           setUser(data);
           setReady(true);
